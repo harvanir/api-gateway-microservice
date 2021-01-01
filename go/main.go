a1383c4a0e6861dcaca07fa1c84f5a32a17aa541
Harvan Irsyadi <harvanir@gmail.com>
2021-01-01 21:47:44 +0700

    Polishing

--- go/main.go
+++ go/main.go
@@ -23,10 +23,14 @@ func main() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["name"]
-	delay, err := strconv.Atoi(r.URL.Query().Get("delay"))
+	delayParArr, ok := r.URL.Query()["delay"]
 
-	if err == nil && delay > 0 {
-		time.Sleep(time.Duration(delay) * time.Millisecond)
+	if ok && len(delayParArr[0]) > 0 {
+		delay, err := strconv.Atoi(delayParArr[0])
+
+		if err == nil && delay > 0 {
+			time.Sleep(time.Duration(delay) * time.Millisecond)
+		}
 	}
 
 	fmt.Fprintf(w, "Hello "+key)
--- php/src/hello.php
+++ php/src/hello.php
@@ -1,5 +1,16 @@
 <?php
 header_remove();
 header('Content-Type: text/plain');
+
+$delayStr = $_GET["delay"];
+
+if ($delayStr != null) {
+    $delay = intval($delayStr);
+
+    if ($delay > 0) {
+        usleep($delay * 1000);
+    }
+}
+
 echo 'Hello ' . htmlspecialchars($_GET["name"]);
 ?>
